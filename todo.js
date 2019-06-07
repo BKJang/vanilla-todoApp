931e2f5f628232036115cce067896b5e4902a3f9
BKJang <jp302119@gmail.com>
2019-06-08 03:21:34 +0900

    :banana: todoList 삭제 기능 구현 ( event delegation 사용 )

--- todo.js
+++ todo.js
@@ -4,17 +4,51 @@ const toDoList = document.querySelector('.js-toDoList')
 
 const TODO_LIST = 'toDos';
 
+let toDos = [];
+
+function filterToDo(toDo, targetId) {
+    return toDo.id !== Number(targetId);
+}
+
+function deleteToDo(event) {
+    if(event.target.tagName === 'BUTTON') {
+        const targetId = event.target.parentNode.id;
+        const li = event.target.parentNode;
+
+        toDoList.removeChild(li);
+
+        const cleanToDos = toDos.filter((item) => { return filterToDo(item, targetId) })
+        toDos = cleanToDos;
+        saveToDo();
+    }
+}
+
+function saveToDo() {
+    localStorage.setItem(TODO_LIST, JSON.stringify(toDos));
+}
+
 function paintToDo(value) {
     const li = document.createElement('li');
     const deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'X';
     const span = document.createElement('span');
+    const toDoId = toDos.length + 1;
+
+    deleteBtn.innerText = 'X';
     span.innerText = value;
-    
+
     li.appendChild(span);
     li.appendChild(deleteBtn);
+    li.id = toDoId;
 
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        id : toDoId,
+        todo : value,
+    }
+
+    toDos.push(toDoObj);
+    saveToDo();
 }
 
 function handleSubmit(event) {
@@ -24,16 +58,23 @@ function handleSubmit(event) {
     toDoInput.value = '';
 }
 
-function loadTodos() {
-    const toDos = localStorage.getItem(TODO_LIST);
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODO_LIST);
 
-    if(toDos) {
-
+    if(loadedToDos) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.map((item) => {
+            return paintToDo(item.todo);
+        });
     }
+
+    toDoList.addEventListener('click', function(event) {
+        deleteToDo(event);
+    });
 }
 
 function init() {
-    loadTodos();
+    loadToDos();
     toDoForm.addEventListener('submit', handleSubmit);
 }
 
